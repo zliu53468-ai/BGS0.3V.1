4ac122ffbd7337471f9d559c494bd19df3936f9c
zliu53468-ai <zliu53468@gmail.com>
2026-09-01 03:55:50 +0800

    Improve Windows local tester startup

--- start_local_tester.bat
+++ start_local_tester.bat
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 cd /d %~dp0
-start "" http://127.0.0.1:8765
+start "BGS Browser" cmd /c "timeout /t 2 /nobreak >nul & start http://127.0.0.1:8765"
 python -m uvicorn local_road_test_app:app --host 127.0.0.1 --port 8765
 if errorlevel 1 (
   echo.
